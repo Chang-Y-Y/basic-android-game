a8fc4a08ac0848bd3c3491220a11d3522a706f3b
Chang Yeob Yun <changy@sfu.ca>
2020-10-18 04:59:06 -0700

    Turned keys into constant in OptionsActivity

--- app/src/main/java/ca/cmpt276/as3/OptionsActivity.java
+++ app/src/main/java/ca/cmpt276/as3/OptionsActivity.java
@@ -2,6 +2,7 @@ package ca.cmpt276.as3;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -18,7 +19,23 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import ca.cmpt276.as3.model.OptionsConfig;
+
+/**
+ *  This activity is the screen that lets the user config the game settings
+ *  Creates the radio buttons and saves the options in the OptionsConfig class object
+ *  and shared preferences
+ */
 public class OptionsActivity extends AppCompatActivity {
+
+
+    private static final String NUM_ROWS_KEY = "Num rows for game board";
+    private static final String NUM_COLS_KEY = "Num cols for game board";
+    private static final int DEFAULT_NUM_BUGS = 6;
+    private static final int DEFAULT_NUM_ROWS = 6;
+    private static final int DEFAULT_NUM_COLS = 4;
+    public static final String NUM_BUGS_KEY = "Num bugs in game";
+    private OptionsConfig optionsConfig;
 
     // Setting color of the radio buttons https://stackoverflow.com/questions/17120199/change-circle-color-of-radio-button/41516331
     private ColorStateList colorStateList = new ColorStateList(
@@ -43,6 +60,8 @@ public class OptionsActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        optionsConfig = OptionsConfig.getInstance();
+
         // Set up Radio Options
         setupNumBugsRadioButtons();
         setupBoardDimensionRadioButtons();
@@ -64,28 +83,41 @@ public class OptionsActivity extends AppCompatActivity {
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-
+                    saveNumBug(numBug);
                 }
             });
 
             // Add to radio group
             radioGroup.addView(button);
 
-            if (i == 0) {
-                radioGroup.check(button.getId());
+            if (numBug == getNumBugInGame(this)) {
+                button.setChecked(true);
             }
         }
+    }
+
+    private void saveNumBug(int numBug) {
+        SharedPreferences prefs = this.getSharedPreferences("AppPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(NUM_BUGS_KEY, numBug);
+        editor.apply();
+        optionsConfig.setNumBug(numBug);
+    }
+
+    static public int getNumBugInGame(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("AppPrefs", MODE_PRIVATE);
+        return prefs.getInt(NUM_BUGS_KEY, DEFAULT_NUM_BUGS);
     }
 
     private void setupBoardDimensionRadioButtons() {
         RadioGroup radioGroup = findViewById(R.id.radio_group_board_dimensions);
 
-        int[] numCols = getResources().getIntArray(R.array.num_cols);
+        final int[] numCols = getResources().getIntArray(R.array.num_cols);
         int[] numRows = getResources().getIntArray(R.array.num_rows);
 
         for (int i = 0; i < numCols.length; i++) {
-            int width = numCols[i];
-            int height = numRows[i];
+            final int width = numCols[i];
+            final int height = numRows[i];
 
             RadioButton button = new RadioButton(this);
             button.setText(getString(R.string.board_dimension, height, width));
@@ -94,17 +126,40 @@ public class OptionsActivity extends AppCompatActivity {
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-
+                    saveBoardSize(height, width);
                 }
             });
 
-
             radioGroup.addView(button);
 
-            if (i == 0) {
-                radioGroup.check(button.getId());
+            if (height == getNumRowsForBoard(this) &&
+                    width == getNumColsForBoard(this)) {
+                button.setChecked(true);
             }
         }
+    }
 
+    private void saveBoardSize(int numRows, int numCols) {
+        SharedPreferences sharedPreferences = this.getSharedPreferences("AppPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(NUM_ROWS_KEY, numRows);
+        editor.putInt(NUM_COLS_KEY, numCols);
+        editor.apply();
+        optionsConfig.setNumRow(numRows);
+        optionsConfig.setNumCol(numCols);
+    }
+
+    static public int getNumRowsForBoard(Context context) {
+        SharedPreferences preferences = context.getSharedPreferences("AppPrefs", MODE_PRIVATE);
+
+        return preferences.getInt(NUM_ROWS_KEY,
+                DEFAULT_NUM_ROWS);
+    }
+
+    static public int getNumColsForBoard(Context context) {
+        SharedPreferences preferences = context.getSharedPreferences("AppPrefs", MODE_PRIVATE);
+
+        return preferences.getInt(NUM_COLS_KEY,
+                DEFAULT_NUM_COLS);
     }
 }
---
+++ app/src/main/java/ca/cmpt276/as3/model/OptionsConfig.java
@@ -0,0 +1,48 @@
+package ca.cmpt276.as3.model;
+
+public class OptionsConfig {
+
+    private int numBug;
+    private int numRow;
+    private int numCol;
+
+    private static OptionsConfig instance;
+
+    private OptionsConfig() {
+
+    }
+
+    public static OptionsConfig getInstance() {
+        if (instance == null) {
+            instance = new OptionsConfig();
+            instance.setNumBug(6);
+            instance.setNumRow(4);
+            instance.setNumCol(6);
+        }
+        return instance;
+    }
+
+    public int getNumBug() {
+        return numBug;
+    }
+
+    public void setNumBug(int numBug) {
+        this.numBug = numBug;
+    }
+
+    public int getNumRow() {
+        return numRow;
+    }
+
+    public void setNumRow(int numRow) {
+        this.numRow = numRow;
+    }
+
+    public int getNumCol() {
+        return numCol;
+    }
+
+    public void setNumCol(int numCol) {
+        this.numCol = numCol;
+    }
+}
