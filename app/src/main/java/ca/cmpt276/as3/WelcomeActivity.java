87d3f911e2bac9be47ce79d3a776d3718d2d3a6a
Chang Yeob Yun <changy@sfu.ca>
2020-10-21 03:48:07 -0700

    Added animation for welcome screen and also skip functionality

--- app/src/main/java/ca/cmpt276/as3/WelcomeActivity.java
+++ app/src/main/java/ca/cmpt276/as3/WelcomeActivity.java
@@ -2,6 +2,7 @@ package ca.cmpt276.as3;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -10,11 +11,21 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.Handler;
+import android.view.Display;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    Handler handler;
+    Runnable runnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +36,17 @@ public class WelcomeActivity extends AppCompatActivity {
 
         // Set up button to skip to main menu
         setUpMainMenuButton();
+        setUpAnimations();
+
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                gotoMainMenu();
+            }
+        };
+        handler.postDelayed(runnable, 5000);
+
     }
 
     public void gotoMainMenu() {
@@ -38,9 +60,26 @@ public class WelcomeActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                handler.removeCallbacks(runnable);
                 gotoMainMenu();
             }
         });
+    }
+
+    public void setUpAnimations() {
+        TextView textView = findViewById(R.id.text_game_title);
+        AlphaAnimation fadeIn = new AlphaAnimation(0.0f, 1.0f);
+        textView.startAnimation(fadeIn);
+        fadeIn.setDuration(5000);
+        fadeIn.setFillAfter(true);;
+
+
+        ImageView imageView = findViewById(R.id.image_welcome_image);
+        RotateAnimation rotateAnimation = new RotateAnimation(0, 360,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+        imageView.startAnimation(rotateAnimation);
+        rotateAnimation.setDuration(5000);
     }
 
 }
